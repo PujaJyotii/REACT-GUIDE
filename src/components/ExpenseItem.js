ce8c15a9194822dca4c4e7871c4c071f6797d91a
PujaJyotii <pujajyoti111>
2023-08-19 14:44:21 +0530

    first commit

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,7 @@
+import './ExpenseItem.css';
+
+function ExpenseItem() {
+    return <div className='expense-item'>
+        <div>August 19 2023</div>
+        <div className='expense-item_description'>
+            
