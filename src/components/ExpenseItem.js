0676f7fc92f9136e6c256bcaeda26929e9fc54f5
PujaJyotii <pujajyoti111>
2023-08-19 15:25:52 +0530

    hardCode conversion

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,7 +1,19 @@
 import './ExpenseItem.css';
 
 function ExpenseItem() {
+    const expenseDate=new Date(2023,7,19);
+    const expenseTitle='Car insurance';
+    const expenseAmount=294.67;
+    const expenseLocation='office'
     return <div className='expense-item'>
-        <div>August 19 2023</div>
+        <div>{expenseDate.toISOString()}</div>
         <div className='expense-item_description'>
-            
+            <h2>{expenseTitle}</h2>
+            <div className='expense-item_price'>${expenseAmount}</div>
+            <div className='expense-item_location'>{expenseLocation}</div>
+        </div>
+    </div> 
+
+}
+
+export default ExpenseItem;
